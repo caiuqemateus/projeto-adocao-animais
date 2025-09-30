a7356b99894a58fa7e214f046a6cda6ec2f2d7d2
jao1311 <145819511+jao1311@users.noreply.github.com>
2025-09-30 15:26:31 -0300

    L

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ npx prisma dev --name init
 'npx prisma db push' --force-reset
 npx prisma studio
 npm run dev
+npm install bcrypt express-session nodmailer uuid
--- src/api.js
+++ src/api.js
@@ -38,7 +38,7 @@ app.use((err, _req, res, _next) => {
     res.status(500).json({error: 'Erro interno'});
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log (`http://localhost:${PORT}`));
 
 
