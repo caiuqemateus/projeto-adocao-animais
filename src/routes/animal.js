21eda683b449bd4e36b03347d8a4f22f4276d673
jao1311 <145819511+jao1311@users.noreply.github.com>
2025-10-09 16:48:23 -0300

    acho q ta feita a groups dos animal

--- src/routes/animal.js
+++ src/routes/animal.js
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import { AnimalController } from '../controllers/Animal.js';
 import {verificaToken} from '../middlewares/auth.js';
+import { verificaRole } from '../middlewares/roles.js';
 
 const route = Router();
 
-route.post('/',verificaToken ,AnimalController.store);
-route.get('/:id',AnimalController.show);
+route.post('/',verificaToken,verificaRole(["POBRE", "EDITOR", "ADMIN"]) ,AnimalController.store);
+route.get('/:id',verificaRole(["VIEWER"]), AnimalController.show);
 route.delete('/:id',AnimalController.del);
-route.put('/:id',verificaToken ,AnimalController.upd);
+route.put('/:id',verificaToken ,verificaRole(["POBRE", "EDITOR", "ADMIN"]) ,AnimalController.upd);
 route.get('/',AnimalController.index);
 
 
