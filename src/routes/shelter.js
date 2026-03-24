ff7f16ef7237643e367d99c702537ed9cc31d838
Rafaela Saraiva <rafaela.saraiva100@hotmail.com>
2026-03-24 16:49:36 -0300

    a ong ja esta cadastrando, mas não está logando pois precisa mexer no prisma

--- src/routes/shelter.js
+++ src/routes/shelter.js
@@ -4,10 +4,10 @@ import {verificaToken} from '../middlewares/auth.js';
 import { verificaRole } from '../middlewares/roles.js';
 const route = Router();
 
-route.post('/',verificaToken,verificaRole(["EDITOR"]), ShelterController.store);
+route.post('/', ShelterController.store);
 route.get('/:id',verificaToken,verificaRole(["VIEWER"]), ShelterController.show);
-route.delete('/:id', ShelterController.del);
-route.put('/:id', ShelterController.upd);
+route.delete('/:id', verificaToken, verificaRole(["EDITOR"]), ShelterController.del);
+route.put('/:id', verificaToken, verificaRole(["EDITOR"]), ShelterController.upd);
 route.get('/', ShelterController.index);
 
 
